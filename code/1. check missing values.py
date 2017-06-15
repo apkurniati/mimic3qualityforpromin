19c9302e0673774e1e68118187ce4c092ba1fa24
Angelina Prima Kurniati <scapk@leeds.ac.uk>
2017-06-15 10:57:38 +0100

    Create 1. check missing values.py

--- code/1. check missing values.py
+++ code/1. check missing values.py
@@ -4,15 +4,17 @@ Created on Thu Jun  1 10:02:15 2017
 @author: scapk
 """
 import pandas as pd
+import glob
 
-file = 'm3dq/adm.csv'
-df = pd.read_csv(file, header=0, sep=';', index_col=0, parse_dates=True, encoding=None, tupleize_cols=False, infer_datetime_format=False)
-#print summary
-print('Summary of '+file+': ')
-print(df.describe())
-#counting missing values per columns
-print('Count of missing values per columns in '+file+': ')
-print(df.isnull().sum())
-#row count
-print('Total row count in '+file+': ')
-print(len(df))
+#file = 'mimic3/ADMISSIONS.csv'
+def count_missing(file):
+    df = pd.read_csv(file, header=0, sep=',', index_col=0)
+    #counting missing values per columns
+    print('Count of missing values per columns in '+ file +': ')
+    print(df.isnull().sum())
+    #row count
+    print('Total row count in '+ file +': ')
+    print(len(df))
+
+for f in glob.glob('mimic3/*.csv'):
+    count_missing(f)
